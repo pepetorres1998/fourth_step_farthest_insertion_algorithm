559fd6bf8babb37e37b521ab858636bd18726c32
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2020-04-30 11:49:09 -0500

    add fourth step completed

--- 4th_step.rb
+++ 4th_step.rb
@@ -1,9 +1,26 @@
+require 'csv'
+require 'pry'
+
 def main
-  csv = CSV.read("./distances.csv")
-  tour = [1, 2, 3, 1]
-  k = 5
-  where_to_insert = 4thstep(tour, distances)
+  distances = CSV.read("./distances.csv")
+  tour = [1, 5, 1]
+  k = 4
+  where_to_insert = fourth_step(k, tour, distances)
+  puts "Se inserta entre el nodo #{where_to_insert[0]}, y el nodo #{where_to_insert[1]}"
 end
 
-def 4thstep(tour, distances)
+def fourth_step(k, tour, distances)
+  paths = tour.map.with_index { |node, index|
+    [tour[index], tour[index+1]] unless index == tour.length - 1
+  }[0..-2]
+
+  paths_delta_f = paths.map { |path|
+    distances[path[0] - 1][k - 1].to_i + distances[k - 1][path[1] - 1].to_i - distances[path[0] - 1][path[1] - 1].to_i
+  }
+
+  index_of_maximum_distance = paths_delta_f.find_index(paths_delta_f.min)
+
+  paths[index_of_maximum_distance]
 end
+
+main
